ebd7eb0c6ca19e4fd404fa8b04050ab4c380f0dd
fatima1414 <shaikhfatima5260@gmai.com>
2025-04-27 12:18:50 +0530

    code

--- QUESTION 3/code.cpp
+++ QUESTION 3/code.cpp
@@ -1,16 +1,16 @@
   
 //pattern three
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//        int n=5;
-//        for(int row=n; row>=1;row--){
-//         for(int col=1; col<=row; col++){
-//             cout<<(col%2) <<"  ";
-//         }
-//         cout<<endl;
-//        }
-// }
+int main(){
+       int n=5;
+       for(int row=n; row>=1;row--){
+        for(int col=1; col<=row; col++){
+            cout<<(col%2) <<"  ";
+        }
+        cout<<endl;
+       }
+}
 
--- QUESTION 6/code.cpp
+++ QUESTION 6/code.cpp
@@ -1,15 +1,48 @@
 
-// pattern 6
+// // pattern 6
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     for (char ch = 'A'; ch <= 'Z'; ch++)
+//     {
+//         cout << ch << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    for (char ch = 'A'; ch <= 'Z'; ch++)
-    {
-        cout << ch << " ";
+int main() {
+    // First Pattern
+    for (int i = 1; i <= 2; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+        cout << "  "; // Space between the two sections
+        cout << i << endl;
     }
+
+    cout << endl; // Space between patterns
+
+    // Second Pattern
+    cout << "  1 2 3" << endl;  // Printing numbers
+    cout << "1  *   *" << endl;
+    cout << "3  *****" << endl;
+    cout << "2  *" << endl;
+    cout << "1  *" << endl;
     cout << endl;
+
+    // Third Pattern
+    cout << "  1 2 3 4" << endl;  // Printing numbers
+    cout << "4" << endl;
+    cout << "3" << endl;
+    cout << "2" << endl;
+    cout << "1" << endl;
+
     return 0;
 }
